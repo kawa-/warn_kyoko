87b4024f07ef4e9abc3269f324a8de61f7cae88f
KAWAMORITA Mitsuaki <kawa@hongo.wide.ad.jp>
2017-03-28 18:29:26 +0900

    alertは余計な気がするので削除

--- warn_kyoko.js
+++ warn_kyoko.js
@@ -6,5 +6,4 @@
 // @include http://kyoko-np.net/*
 // ==/UserScript==
 
-alert("虚構新聞 (悪質デマサイト、ネットデブリ) を検知しました！");
 document.body.innerHTML = "<p>これは <font color=red>虚構新聞 (悪質デマサイト、ネットデブリ)</font> へのアクセスです！ ブロックしました。 (まれにfooter部分が表示されることがあります。直し方募集中)</p>";
